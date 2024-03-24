86ae0ed95448de0053c18bfc6f554489df79dfbc
Pavel Rogovtsov <progovcov0@gmail.com>
2024-03-24 14:16:57 +0300

    conversion v1

--- src/app/main-activity/conversion/conversion.component.ts
+++ src/app/main-activity/conversion/conversion.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-conversion',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './conversion.component.html',
   styleUrl: './conversion.component.css'
 })
+
 export class ConversionComponent {
+  decimal: number = 0;
+  binary: string = '0';
+  octal: string = '0';
+  hexadecimal: string = '0';
+
+  convertFromDecimal() {
+    const value = this.decimal;
+    this.binary = value.toString(2);
+    this.octal = value.toString(8);
+    this.hexadecimal = value.toString(16).toUpperCase();
+  }
+
+  convertFromBinary() {
+    const value = parseInt(this.binary.toString(), 2);
+    this.decimal = value;
+    this.octal = value.toString(8);
+    this.hexadecimal = value.toString(16).toUpperCase();
+  }
+
+  convertFromOctal() {
+    const value = parseInt(this.octal.toString(), 8);
+    this.decimal = value;
+    this.binary = value.toString(2);
+    this.hexadecimal = value.toString(16).toUpperCase();
+  }
+
+  convertFromHexadecimal() {
+    const value = parseInt(this.hexadecimal.toString(), 16);
+    this.decimal = value;
+    this.binary = value.toString(2);
+    this.octal = value.toString(8);
+  }
 
 }
