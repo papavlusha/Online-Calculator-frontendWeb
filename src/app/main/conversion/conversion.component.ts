50f1f3daf2f6b5a37b355a4988b4e5ca11e32f95
Pavel Rogovtsov <progovcov0@gmail.com>
2024-05-27 14:42:30 +0300

    add lib swap

--- src/app/main/conversion/conversion.component.ts
+++ src/app/main/conversion/conversion.component.ts
@@ -13,44 +13,48 @@ export class ConversionComponent {
   octal: string = '0';
   decimal: string = '0';
   hexadecimal: string = '0';
-
+  lib: string = 'Cpp'
   constructor(private http: HttpClient, private authService: AuthService) {}
 
-  convert(sourceBase: string, number: string) {
-    let lib: string;
-
+  convert(sourceBase: string, number: string, lib: string) {
     this.authService.currentLibrary.subscribe(library => {
+
       if (library === 'Cpp') {
         lib = 'Cpp';
       } else if (library === 'Java') {
         lib = 'Java';
       }
 
-      const request = { sourceBase, number, lib };
-      this.authService.convertNumber(request).subscribe(response => {
-        this.binary = response.binaryNumber;
-        this.decimal = response.decimalNumber;
-        this.octal = response.octalNumber;
-        this.hexadecimal = response.hexadecimalNumber;
-      }, error => {
+      const request = { sourceBase, number, lib};
+
+      this.authService.convertNumber(request).subscribe(
+        response => {
+          this.binary = response.binaryNumber;
+          this.decimal = response.decimalNumber;
+          this.octal = response.octalNumber;
+          this.hexadecimal = response.hexadecimalNumber;
+        },
+        error => {
           console.error('Ошибка при выполнении запроса:', error);
-        });
+        }
+      );
     });
   }
 
+
   convertFromBinary() {
-    this.convert('2', this.binary);
+    this.convert('2', this.binary, this.lib);
   }
 
   convertFromOctal() {
-    this.convert('8', this.octal);
+    this.convert('8', this.octal, this.lib);
   }
 
   convertFromDecimal() {
-    this.convert('10', this.decimal);
+    this.convert('10', this.decimal, this.lib);
   }
 
   convertFromHexadecimal() {
-    this.convert('16', this.hexadecimal);
+    this.convert('16', this.hexadecimal, this.lib);
   }
 }
